66c30da6e5038bd9d2e752bf2686ec4f43f6744e
jhs512 <jangka512@gmail.com>
2021-12-30 12:25:53 +0900

    장고에서 엘라스틱 서치를 통해서 검색

--- products/views.py
+++ products/views.py
@@ -1,7 +1,34 @@
+from django.db.models import When, Case, QuerySet
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
-
 # Create your views here.
+from elasticsearch import Elasticsearch
+
+from products.models import Product
+
+
 def product_list(request: HttpRequest):
-    return HttpResponse("안녕")
+    search_keyword = request.GET.get("search_keyword", "")
+
+    elasticsearch = Elasticsearch("http://192.168.56.101:9200", http_auth=("elastic", "elasticpassword"))
+    ela_query_result = elasticsearch.sql.query(body={
+        "query": f"""
+        SELECT id
+        FROM ela___products_product___v1
+        WHERE MATCH(display_name, '{search_keyword}') OR MATCH(name, '{search_keyword}')
+        ORDER BY score() DESC
+        """
+    })
+
+    print(ela_query_result)
+
+    ids = [row[0] for row in ela_query_result['rows']]
+
+    order = Case(*[When(id=id, then=pos) for pos, id in enumerate(ids)])
+    qs: QuerySet = Product.objects.filter(id__in=ids).order_by(order)
+
+    product_dict_list = [{"id": product.id, "name": product.name, "display_name": product.display_name} for product in
+                         qs]
+
+    return HttpResponse(product_dict_list)
